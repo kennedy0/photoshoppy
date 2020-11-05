f06572babe294e600a5c15ca5cbf6bc2f868f0c5
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2020-11-05 14:05:40 -0800

    Removed some duplication

--- photoshoppy/psd_render/render_utils.py
+++ photoshoppy/psd_render/render_utils.py
@@ -8,15 +8,18 @@ from photoshoppy.utilities.array import crop_array, pad_array
 
 def layer_to_screen_space(layer: Layer, psd: PSDFile) -> np.array:
     """ Return a Layer's image data in screen space. """
-    bbox = Rect(0, 0, psd.height, psd.width)
-    cropped_image_data = crop_array(array=layer.image_data, rect=layer.rect, bbox=bbox)
-    ss_image_data = pad_array(array=cropped_image_data, rect=layer.rect, width=psd.width, height=psd.height)
-    return ss_image_data
+    return _image_to_screen_space(layer.image_data, layer.rect, psd.width, psd.height)
 
 
 def mask_to_screen_space(layer: Layer, psd: PSDFile) -> np.array:
     """ Return a Layer's mask in screen space. """
-    bbox = Rect(0, 0, psd.height, psd.width)
-    cropped_image_data = crop_array(array=layer.layer_mask.image_data, rect=layer.rect, bbox=bbox)
-    ss_image_data = pad_array(array=cropped_image_data, rect=layer.rect, width=psd.width, height=psd.height)
+    if layer.layer_mask is None:
+        raise RuntimeError(f"Layer '{layer.name}' has no layer mask; cannot convert to screen space.")
+    return _image_to_screen_space(layer.layer_mask.image_data, layer.rect, psd.width, psd.height)
+
+
+def _image_to_screen_space(image_data: np.array, image_rect: Rect, width: int, height: int) -> np.array:
+    bbox = Rect(0, 0, height, width)
+    cropped_image_data = crop_array(array=image_data, rect=image_rect, bbox=bbox)
+    ss_image_data = pad_array(array=cropped_image_data, rect=image_rect, width=width, height=height)
     return ss_image_data
