ce30859e8779451b24ba7c4087cb77f03ac74757
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2020-11-04 18:08:35 -0800

    Added commandline args to test_render_psd.py

--- tests/test_render_psd.py
+++ tests/test_render_psd.py
@@ -1,4 +1,6 @@
 import os
+import sys
+from typing import List
 
 from photoshoppy.psd_file import PSDFile
 from photoshoppy.psd_render.render import render_psd
@@ -10,7 +12,7 @@ PSD_FILE_PATH = os.path.join(THIS_DIR, "psd_files", "zig_zags.psd")
 OUTPUT_FILE_PATH = os.path.join(OUTPUT_DIR, "zig_zags.png")
 
 
-def main():
+def main(files: List[str]):
     try:
         os.makedirs(OUTPUT_DIR)
     except OSError:
@@ -21,10 +23,17 @@ def main():
     except OSError:
         pass
 
-    psd = PSDFile(PSD_FILE_PATH)
-    print(f"rendering {OUTPUT_FILE_PATH}")
-    render_psd(psd, OUTPUT_FILE_PATH, overwrite=True)
+    if not len(files):
+        files = [PSD_FILE_PATH]
+
+    for file in files:
+        psd = PSDFile(file)
+        output_file_name = os.path.splitext(os.path.basename(file))[0] + ".png"
+        output_file_path = os.path.join(OUTPUT_DIR, output_file_name)
+        print(f"rendering {output_file_path}")
+        render_psd(psd, output_file_path, overwrite=True)
 
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv[1:]
+    main(args)
