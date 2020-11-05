fe973147707843b7ea04a41315929162f856d434
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2020-11-05 13:09:59 -0800

    kwarg re-order

--- photoshoppy/psd_render/render_utils.py
+++ photoshoppy/psd_render/render_utils.py
@@ -10,7 +10,7 @@ def layer_to_screen_space(layer: Layer, psd: PSDFile) -> np.array:
     """ Return a Layer's image data in screen space. """
     bbox = Rect(0, 0, psd.height, psd.width)
     cropped_image_data = crop_array(array=layer.image_data, rect=layer.rect, bbox=bbox)
-    ss_image_data = pad_array(array=cropped_image_data, rect=layer.rect, height=psd.height, width=psd.width)
+    ss_image_data = pad_array(array=cropped_image_data, rect=layer.rect, width=psd.width, height=psd.height)
     return ss_image_data
 
 
